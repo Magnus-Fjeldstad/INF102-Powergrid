24162f086fee986e0324c739b69d1d198322bf49
Mafje8943 <mafje8943@uib.no>
2023-10-24 12:45:43 +0200

    working 1,2 and 5/7 3

--- src/main/java/student/ProblemSolver.java
+++ src/main/java/student/ProblemSolver.java
@@ -1,11 +1,17 @@
 package student;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
 import java.util.Stack;
 
 import graph.*;
@@ -71,6 +77,7 @@ public class ProblemSolver implements IProblem {
         return null;
     }
 
+    // Non recursive dfs
     private <V> HashMap<V, V> dfs(Graph<V> g, V root) {
         HashMap<V, V> parentMap = new HashMap<>();
         HashSet<V> visited = new HashSet<>();
@@ -92,52 +99,122 @@ public class ProblemSolver implements IProblem {
         }
         return parentMap;
     }
+
     @Override
     public <V> Edge<V> addRedundant(Graph<V> g, V root) {
-        UnionFindBySize<V> uf = new UnionFindBySize<>(g.vertices());
+        LinkedList<Graph<V>> Trees = biggestSubTreeList(g, root);
 
+        Graph<V> firstBiggestTree = Trees.poll();
+        Graph<V> secondBiggestTree = Trees.poll();
 
-        for (Edge<V> edge : g.edges()) {
-            uf.union(edge.a, edge.b);
+        V node1 = null;
+        V node2 = null;
+
+        if (firstBiggestTree != null) {
+            node1 = getDeepestNodeWithMostNeighbours(firstBiggestTree);
         }
 
-
-        HashMap<Edge<V>, Integer> impactMap = new HashMap<>();
-        for (Edge<V> edge : g.edges()) {
-            g.removeEdge(edge);
-            if (!uf.same(edge.a, edge.b)) {
-                int impact = ((Graph<V>) uf.group(edge.a)).size() + ((Graph<V>) uf.group(edge.b)).size();
-                impactMap.put(edge, impact);
-            }
-            g.addEdge(edge);
+        if (secondBiggestTree != null) {
+            node2 = getDeepestNodeWithMostNeighbours(secondBiggestTree);
         }
 
+        Edge<V> edge;
+        if (node1 != null && node2 != null) {
+            // Both subtrees exist
+            edge = new Edge<V>(node1, node2);
+        } else if (node1 != null) {
+            // Only the first subtree exists
+            edge = new Edge<V>(root, node1);
+        } else if (node2 != null) {
+            // Only the second subtree exists
+            edge = new Edge<V>(root, node2);
+        } else {
+            // No subtrees exist, handle this case as necessary
+            throw new IllegalStateException("No subtrees found");
+        }
+        return edge;
+    }
 
-        Edge<V> bestEdge = null;
-        int minImpact = Integer.MAX_VALUE;
-        for (V u : g.vertices()) {
-            for (V v : g.vertices()) {
-                if (u.equals(v) || g.adjacent(u, v))
-                    continue;
+    // fester til groveste foreldrer
+    private static <V> V getDeepestNodeWithMostNeighbours(Graph<V> graph) {
+        Map<V, Integer> depthMap = new HashMap<>();// map med dybden til en node
+        Queue<V> queue = new LinkedList<>();
+        V root = graph.getFirstNode();// rooten
+        queue.add(root);// legger til root i køen
+        depthMap.put(root, 0);// root har dybde 0
 
+        int maxDepth = 0; // max depth settes til 0
 
-                g.addEdge(u, v);
-                int maxImpactAfterAdding = 0;
-                for (Edge<V> criticalEdge : impactMap.keySet()) {
-                    g.removeEdge(criticalEdge);
-                    int impact = !uf.same(criticalEdge.a, criticalEdge.b) ? ((Graph<V>) uf.group(criticalEdge.a)).size()
-                            + ((Graph<V>) uf.group(criticalEdge.b)).size() : 0;
-                    maxImpactAfterAdding = Math.max(maxImpactAfterAdding, impact);
-                    g.addEdge(criticalEdge);
+        // bryr oss bare om dybden
+        while (!queue.isEmpty()) {// imens køen ikke er tom
+            V currentNode = queue.poll();// henter og fjerner frøste node fra kjøen
+            for (V neighbour : graph.neighbours(currentNode)) {// for naboene til current
+                if (!depthMap.containsKey(neighbour)) {// hvis naboen ikke finnes i depthMap
+                    int depth = depthMap.get(currentNode) + 1;// dybden plusses med 1
+                    depthMap.put(neighbour, depth);// legger til naboen og dybden i hashmap
+                    queue.add(neighbour);// legger til naboen i køen
+                    if (depth > maxDepth) {// om dybden er større en max
+                        maxDepth = depth; // blir max den gitte dybden
+                    }
                 }
-                if (maxImpactAfterAdding < minImpact) {
-                    minImpact = maxImpactAfterAdding;
-                    bestEdge = new Edge<>(u, v);
-                }
-                g.removeEdge(u, v);
             }
         }
-        return bestEdge;
+
+        V champNode = null;// seiers node
+        int maxNeighbours = 0;// highscore variabel
+
+        for (Map.Entry<V, Integer> entry : depthMap.entrySet()) {// entry gjør det lett å iterere over
+            if (entry.getValue() == maxDepth - 1) {// bare noder som har max dybde, minus 1 så får jeg foreldre
+                int currentNeighbourCount = graph.degree(entry.getKey());// teller naboene til en gitt node
+                if (currentNeighbourCount > maxNeighbours) {// hvis currentGradtall er større en maxGradtall
+                    maxNeighbours = currentNeighbourCount;// settes currentGradtall til maxGradtall
+                    champNode = entry.getKey();// champNode blir kandidat for noden som skal returneres
+                }
+            }
+        }
+
+        return champNode;
+    }
+
+    private <V> LinkedList<Graph<V>> biggestSubTreeList(Graph<V> g, V root) {
+        LinkedList<Graph<V>> treeList = new LinkedList<>();
+        for (V node : g.neighbours(root)) {
+            treeList.add(bfsSubTree(node, g, root));
+        }
+
+        treeList.sort(Comparator.comparingInt((Graph<V> graph) -> graph.numVertices()).reversed());
+
+        return treeList;
+    }
+
+    // må være feil her
+    private <V> Graph<V> bfsSubTree(V startNode, Graph<V> g, V root) {// lagger subtree fra en gitt node. Vil at den
+                                                                      // skal finne
+        Graph<V> subTree = new Graph<>();// nytt tomt tree
+        subTree.addVertex(startNode);// legger til ny root
+
+        Set<V> visited = new HashSet<>();// tomt hashset
+        Queue<V> queue = new LinkedList<>();// tom queue
+
+        visited.add(startNode);// legger til startNode i visited
+        queue.add(startNode);// legger til startNode i køen
+
+        while (!queue.isEmpty()) {// i mens køen ikke er tom
+            V current = queue.poll();// current er køens førstemann
+
+            for (V neighbor : g.neighbours(current)) {// for barna til current
+                if (!current.equals(neighbor) && !visited.contains(neighbor) && !current.equals(root)) {// hvis naboene
+                                                                                                        // ikke er
+                                                                                                        // besøkt
+                    visited.add(neighbor);// legg til nabo i besøkt
+                    queue.add(neighbor);// legg til nabo i kø
+
+                    subTree.addVertex(neighbor);// legger til noden i treet
+                    subTree.addEdge(current, neighbor);// legger til kanten i treet
+                }
+            }
+        }
+        return subTree;
     }
 
 }
